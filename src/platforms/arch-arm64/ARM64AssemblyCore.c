08bec0fc2146cd8d13b5c513297db98fbace9ef1
jmpews <jmpews@gmail.com>
2018-05-27 05:47:33 +0800

    [doing] design AssemblyCore again

--- src/platforms/arch-arm64/ARM64AssemblyCore.c
+++ src/platforms/arch-arm64/ARM64AssemblyCore.c
@@ -10,124 +10,141 @@
 // clang-format off
 // automatic generation
 ARM64InstructionX ARM64InstArrary[1024] = {0};
-OP OPArray[1024] = {0};
 // clang-format on
 
-void intializeAssemblyCore() {
-    uintptr_t opIndex = 0;
-    ARM64InstId id;
-
-    id                              = LoadLiteral;
-    ARM64InstArrary[id].id          = id;
-    ARM64InstArrary[id].isClass     = 1;
-    ARM64InstArrary[id].parentIndex = -1;
-    ARM64CoreINIT(id);
-
-    id                  = LDRWl;
-    ARM64InstArrary[id] = (ARM64InstructionX){id, 0, LoadLiteral, 0, 0};
-    ARM64CoreINIT(id);
-
-    id                     = LDRXl;
-    ARM64InstArrary[LDRXl] = (ARM64InstructionX){id, 0, LoadLiteral, 0, 0};
-    ARM64CoreINIT(id);
-
-    //  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-    id                              = BaseCmpBranch;
-    ARM64InstArrary[id].id          = id;
-    ARM64InstArrary[id].isClass     = 1;
-    ARM64InstArrary[id].parentIndex = -1;
-    ARM64CoreINIT(id);
-
-    id                              = MULTICLASS(CmpBranch, W);
-    ARM64InstArrary[id].id          = id;
-    ARM64InstArrary[id].isClass     = 1;
-    ARM64InstArrary[id].parentIndex = BaseCmpBranch;
-    ARM64CoreINIT(id);
-
-    id                     = MULTICLASS(CBZ, W);
-    ARM64InstArrary[LDRXl] = (ARM64InstructionX){id, 0, MULTICLASS(CmpBranch, W), 0, 0};
-    ARM64CoreINIT(id);
-
-    id                     = MULTICLASS(CBNZ, W);
-    ARM64InstArrary[LDRXl] = (ARM64InstructionX){id, 0, MULTICLASS(CmpBranch, W), 0, 0};
-    ARM64CoreINIT(id);
-
-    id                              = MULTICLASS(CmpBranch, X);
-    ARM64InstArrary[id].id          = id;
-    ARM64InstArrary[id].isClass     = 1;
-    ARM64InstArrary[id].parentIndex = BaseCmpBranch;
-    ARM64CoreINIT(id);
-
-    id                     = MULTICLASS(CBZ, X);
-    ARM64InstArrary[LDRXl] = (ARM64InstructionX){id, 0, MULTICLASS(CmpBranch, X), 0, 0};
-    ARM64CoreINIT(id);
-
-    id                     = MULTICLASS(CBNZ, X);
-    ARM64InstArrary[LDRXl] = (ARM64InstructionX){id, 0, MULTICLASS(CmpBranch, X), 0, 0};
-    ARM64CoreINIT(id);
-
-    //  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-}
-
 void ARM64CoreINIT(ARM64InstId id) {
-    ARM64InstructionX *instX = (ARM64InstructionX *)((uintptr_t)ARM64InstArrary + (int)id * sizeof(ARM64InstructionX));
-    ARM64InstructionX *tmpInstX = instX;
-
+    ARM64InstructionX *instX = (ARM64InstructionX *) ((uintptr_t) ARM64InstArrary +
+                                                      (int) id * sizeof(ARM64InstructionX));
+    int parentId = -1;
     uint32_t bits[4 * WORD_SIZE] = {0}; // trick :) AKA LLVM bits<>
 
     do {
         switch (id) {
-        case LDRWl: {
-            BIT32MASKSET(&instX->inst32, &instX->mask32, 30, 2, 0b00); // opc
-            BIT32MASKSET(&instX->inst32, &instX->mask32, 26, 1, 0);    // V
-        }; break;
-        case LDRXl: {
-            BIT32MASKSET(&instX->inst32, &instX->mask32, 30, 2, 0b01); // opc
-            BIT32MASKSET(&instX->inst32, &instX->mask32, 26, 1, 0);    // V
-        }; break;
-        case LoadLiteral: {
-            BIT32SET(&instX->inst32, 30, 2, bits[30]); // opc
-            BIT32MASKSET(&instX->inst32, &instX->mask32, 27, 3, 0b011);
-            BIT32SET(&instX->inst32, 26, 1, bits[26]); // V
-            BIT32MASKSET(&instX->inst32, &instX->mask32, 24, 2, 0b00);
-            BIT32SET(&instX->inst32, 5, 19, bits[5]); // label
-            BIT32SET(&instX->inst32, 0, 5, bits[0]);  // Rt
-        }; break;
-        case MULTICLASS(CBZ, W):
-            BIT32MASKSET(&instX->inst32, &instX->mask32, 24, 1, 0); // op
-            break;
-        case MULTICLASS(CBNZ, W):
-            BIT32MASKSET(&instX->inst32, &instX->mask32, 24, 1, 1); // op
-            break;
-        case MULTICLASS(CmpBranch, W):
-            BIT32MASKSET(&instX->inst32, &instX->mask32, 31, 1, 1);
-            break;
-        case MULTICLASS(CBZ, X):
-            BIT32MASKSET(&instX->inst32, &instX->mask32, 24, 1, 0); // op
-            break;
-        case MULTICLASS(CBNZ, X):
-            BIT32MASKSET(&instX->inst32, &instX->mask32, 24, 1, 1); // op
-            break;
-        case MULTICLASS(CmpBranch, X):
-            BIT32MASKSET(&instX->inst32, &instX->mask32, 31, 1, 0);
-            break;
-        case BaseCmpBranch:
-            BIT32MASKSET(&instX->inst32, &instX->mask32, 25, 6, 0b011010);
-            BIT32SET(&instX->inst32, 24, 1, bits[24]);
-            BIT32SET(&instX->inst32, 5, 19, bits[5]);
-            BIT32SET(&instX->inst32, 0, 5, bits[0]);
-            break;
+            case LDRWl:
+                parentId = LoadLiteral;
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 30, 2, 0b00); // opc
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 26, 1, 0);    // V
+                break;
+            case LDRXl:
+                parentId = LoadLiteral;
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 30, 2, 0b01); // opc
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 26, 1, 0);    // V
+                break;
+            case LoadLiteral:
+                parentId = UNKNOWN;
+                BIT32SET(&instX->inst32, 30, 2, bits[30]); // opc
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 27, 3, 0b011);
+                BIT32SET(&instX->inst32, 26, 1, bits[26]); // V
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 24, 2, 0b00);
+                BIT32SET(&instX->inst32, 5, 19, bits[5]); // label
+                BIT32SET(&instX->inst32, 0, 5, bits[0]);  // Rt
+                break;
+
+            case MULTICLASS(CBZ, W):
+                parentId = MULTICLASS(CmpBranch, W);
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 24, 1, 0); // op
+                break;
+            case MULTICLASS(CBNZ, W):
+                parentId = MULTICLASS(CmpBranch, W);
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 24, 1, 1); // op
+                break;
+            case MULTICLASS(CmpBranch, W):
+                parentId = BaseCmpBranch;
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 31, 1, 0);
+                break;
+            case MULTICLASS(CBZ, X):
+                parentId = MULTICLASS(CmpBranch, X);
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 24, 1, 0); // op
+                break;
+            case MULTICLASS(CBNZ, X):
+                parentId = MULTICLASS(CmpBranch, X);
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 24, 1, 1); // op
+                break;
+            case MULTICLASS(CmpBranch, X):
+                parentId = BaseCmpBranch;
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 31, 1, 0);
+                break;
+            case BaseCmpBranch:
+                parentId= UNKNOWN;
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 25, 6, 0b011010);
+                BIT32SET(&instX->inst32, 24, 1, bits[24]); // op
+                BIT32SET(&instX->inst32, 5, 19, bits[5]); // target
+                BIT32SET(&instX->inst32, 0, 5, bits[0]); // Rt
+                break;
+
+            case Bcc:
+                parentId= BranchCond;
+                break;
+            case BranchCond:
+                parentId= UNKNOWN;
+                BIT32MASKSET(&instX->inst32, &instX->mask32,24, 8, 0b01010100);
+                BIT32SET(&instX->inst32, 5, 19, bits[5]); // target
+                BIT32MASKSET(&instX->inst32, &instX->mask32,4, 1, 0);
+                BIT32SET(&instX->inst32, 0, 4, bits[0]); //cond
+                break;
+
+            case MULTICLASS(TBZ, W):
+                parentId= MULTICLASS(TestBranch, W);
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 24, 1, 0); // op
+                break;
+            case MULTICLASS(TBNZ, W):
+                parentId= MULTICLASS(TestBranch, W);
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 24, 1, 1); // op
+                break;
+            case MULTICLASS(TestBranch, W):
+                parentId= BaseTestBranch;
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 31, 1, 0);
+                break;
+            case MULTICLASS(TBZ, X):
+                parentId= MULTICLASS(TestBranch, X);
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 24, 1, 0); // op
+                break;
+            case MULTICLASS(TBNZ, X):
+                parentId= MULTICLASS(TestBranch, X);
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 24, 1, 1); // op
+                break;
+            case  MULTICLASS(TestBranch, X):
+                parentId= BaseTestBranch;
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 31, 1, 0);
+                break;
+            case BaseTestBranch:
+                parentId= UNKNOWN;
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 25, 6, 0b011011);
+                BIT32SET(&instX->inst32, 24, 1, bits[24]); // op
+                BIT32SET(&instX->inst32, 19, 4, bits[19]); // bits_19_4
+                BIT32SET(&instX->inst32, 5, 14, bits[5]); // target
+                BIT32SET(&instX->inst32, 0, 5, bits[0]); // Rt
+                break;
+
+            case B:
+                parentId= BranchImm;
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 31, 1, 0); // op
+                break;
+            case BranchImm:
+                parentId= BImm;
+                break;
+            case BL:
+                parentId= CallImm;
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 31, 1, 1); // op
+                break;
+            case CallImm:
+                parentId= BImm;
+                break;
+            case BImm:
+                BIT32SET(&instX->inst32, 31, 1, bits[31]); // op
+                BIT32MASKSET(&instX->inst32, &instX->mask32, 26, 5, 0b00101);
+                BIT32SET(&instX->inst32, 0, 26, bits[0]); // addr
+                break;
         }
-        id       = tmpInstX->parentIndex;
-        tmpInstX = (ARM64InstructionX *)((uintptr_t)ARM64InstArrary + (int)id * sizeof(ARM64InstructionX));
-    } while (tmpInstX->parentIndex && instX->inst32);
+        id = parentId;
+    } while (id > 0);
 }
 
 ARM64InstId getInstType(uint32_t inst32) {
     for (int i = 0; i < 1024; i++) {
         ARM64InstructionX *instX =
-            (ARM64InstructionX *)((uintptr_t)ARM64InstArrary + (int)i * sizeof(ARM64InstructionX));
+                (ARM64InstructionX *) ((uintptr_t) ARM64InstArrary +
+                                       (int) i * sizeof(ARM64InstructionX));
         uint32_t bits[4 * WORD_SIZE] = {0}; // trick :) AKA LLVM bits<>
 
         if (instX->inst32 == 0) {
