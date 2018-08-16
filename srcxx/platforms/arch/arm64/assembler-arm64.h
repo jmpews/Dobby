eff12aea8bedbeea28aba506717281229bb89c77
jmpews <jmpews@gmail.com>
2018-08-16 23:36:43 +0800

    x_x

--- srcxx/platforms/arch/arm64/assembler-arm64.h
+++ srcxx/platforms/arch/arm64/assembler-arm64.h
@@ -22,6 +22,14 @@ public:
   void b(int64_t imm26);
   void ldr();
 
+  void Assembler::BranchLink(const StubEntry &stub_entry, Patchability patchable) {
+    const Code &target   = Code::ZoneHandle(stub_entry.code());
+    const int32_t offset = ObjectPool::element_offset(object_pool_wrapper_.FindObject(target, patchable));
+    LoadWordFromPoolOffset(CODE_REG, offset);
+    ldr(TMP, FieldAddress(CODE_REG, Code::entry_point_offset()));
+    blr(TMP);
+  }
+
   void EmitLoadStoreReg(LoadStoreRegOp op, Register rt, Address a, OperandSize sz) {
     const int32_t size     = Log2OperandSizeBytes(sz);
     const int32_t encoding = op | ((size & 0x3) << kSzShift) | Arm64Encode::Rt(rt) | a.encoding();
