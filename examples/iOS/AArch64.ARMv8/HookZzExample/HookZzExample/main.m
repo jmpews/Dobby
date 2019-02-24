f46bd78ae09b5448114f933c9d152631eb251810
jmpews <jmpews@gmail.com>
2019-02-24 21:38:07 +0800

    [example] update examples for multiple arch.

---
+++ examples/iOS/AArch64.ARMv8/HookZzExample/HookZzExample/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  HookZzExample
+//
+//  Created by jmpews on 2019/2/22.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
