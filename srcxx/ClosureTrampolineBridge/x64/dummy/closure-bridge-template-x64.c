d00a5f32c46d427c3275d498303ef4d7aff18ee7
jmpews <jmpews@gmail.com>
2019-03-28 23:06:54 +0800

    [core|x64] update x64 closure bridge, but not done

--- srcxx/ClosureTrampolineBridge/x64/dummy/closure-bridge-template-x64.c
+++ srcxx/ClosureTrampolineBridge/x64/dummy/closure-bridge-template-x64.c
@@ -36,45 +36,35 @@ __attribute__((naked)) void closure_bridge_template() {
   // prepare args
   // @rdi: data_address
   // @rsi: RegisterContext stack address
-  xASM("mov x0, sp");
-  xASM("mov x1, x14");
+  xASM("mov rdi, rsp");
+  xASM("mov rsi, [rsp-16*8]");
   xASM("call " xcdecl("intercept_routing_common_bridge_handler"));
 
   // ======= RegisterContext Restore =======
-  // restore x0
-  xASM("ldr x0, [sp, #8]");
-  xASM("add sp, sp, #(2*8)");
 
-  // restore {x1-x30}
-  xASM("ldp x1, x2, [sp], #16");
-  xASM("ldp x3, x4, [sp], #16");
-  xASM("ldp x5, x6, [sp], #16");
-  xASM("ldp x7, x8, [sp], #16");
-  xASM("ldp x9, x10, [sp], #16");
-  xASM("ldp x11, x12, [sp], #16");
-  xASM("ldp x13, x14, [sp], #16");
-  xASM("ldp x15, x16, [sp], #16");
-  xASM("ldp x17, x18, [sp], #16");
-  xASM("ldp x19, x20, [sp], #16");
-  xASM("ldp x21, x22, [sp], #16");
-  xASM("ldp x23, x24, [sp], #16");
-  xASM("ldp x25, x26, [sp], #16");
-  xASM("ldp x27, x28, [sp], #16");
-  // ldp fp, lr, [sp], #16");
-  xASM("ldp x29, x30, [sp], #16");
+  // general register
+  xASM("pop r15");
+  xASM("pop r14");
+  xASM("pop r13");
+  xASM("pop r12");
+  xASM("pop r11");
+  xASM("pop r10");
+  xASM("pop r9");
+  xASM("pop r8");
+  xASM("pop rsi");
+  xASM("pop rdi");
+  xASM("pop rsp");
+  xASM("pop rbp");
+  xASM("pop rdx");
+  xASM("pop rcx");
+  xASM("pop rbx");
+  xASM("pop rax");
 
-  // restore {q0-q7}
-  xASM("ldp q0, q1, [sp], #32");
-  xASM("ldp q2, q3, [sp], #32");
-  xASM("ldp q4, q5, [sp], #32");
-  xASM("ldp q6, q7, [sp], #32");
+  // flags register
+  xASM("popfq");
 
-  // DO NOT USE epilog
-  // x29 == fp, x30 == lr
-  // xASM("mov sp, x29");
-  // xASM("ldp x29, x30, [sp], #16");
-
-  xASM("br x15");
+  // trick: use the 'carry_data' placeholder, as the return address
+  xASM("ret");
 };
 
 #endif
