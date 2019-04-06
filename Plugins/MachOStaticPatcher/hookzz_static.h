0b86a9134868f7b3ca0fa980a7c52939d6fa5f5f
jmpews <jmpews@gmail.com>
2019-04-06 21:01:40 +0800

    [plugin] update MachOManipulator plugin

--- Plugins/MachOStaticPatcher/hookzz_static.h
+++ Plugins/MachOStaticPatcher/hookzz_static.h
@@ -1,13 +1,13 @@
 #define MAX_STATIC_HOOK_ENTRY 64
 
 typedef struct _InterceptorStatic {
-  void *this_;
-  int count;
+  uint64_t this_;
+  uint64_t count;
   uint64_t entry[MAX_STATIC_HOOK_ENTRY];
 } InterceptorStatic;
 
 typedef struct _HookEntryStatic {
-  int function_offset;
+  uint64_t function;
   uint64_t relocated_origin_function;
   uint64_t *trampoline_target_stub;
 } HookEntryStatic;
