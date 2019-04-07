c34f5709e72449cb542e4c08634f193a3c479f00
jmpews <jmpews@gmail.com>
2019-04-07 21:58:17 +0800

    [plugin] update MachOManipulator plugin

---
+++ Plugins/MachOStaticPatcher/example/MachOStaitcPatcherExample/MachOStaitcPatcherExample/MainLogic.c
@@ -0,0 +1,24 @@
+//
+//  MainLogic.c
+//  MachOStaitcPatcherExample
+//
+//  Created by jmpews on 2019/4/7.
+//  Copyright © 2019 jmpews. All rights reserved.
+//
+
+#include "MainLogic.h"
+
+#include <string.h>
+
+void runMainFunction(int name) {
+  int x, y, z;
+  x = 1;
+  y = 2;
+  z = x + y;
+  
+  int len = strlen(name);
+  
+  z += len;
+  printf("MainFunctionLog %s, len %d.\n", name, z);
+  return;
+}
---
+++ Plugins/MachOStaticPatcher/example/MachOStaitcPatcherExample/MachOStaitcPatcherExample/MainLogic.h
@@ -0,0 +1,16 @@
+//
+//  MainLogic.h
+//  MachOStaitcPatcherExample
+//
+//  Created by jmpews on 2019/4/7.
+//  Copyright © 2019 jmpews. All rights reserved.
+//
+
+#ifndef MainLogic_h
+#define MainLogic_h
+
+#include <stdio.h>
+
+void runMainFunction(int name);
+
+#endif /* MainLogic_h */
