c9d90c6ba1f351e789be1af3c7e37ca755b03eac
jmpews <jmpews@gmail.com>
2019-09-18 19:13:32 +0800

    [plugin] update ZzFindSymbol to support dyld_shared_cache [done]

--- Plugins/FindSymbol/darwin/find_symbol_darwin.c
+++ Plugins/FindSymbol/darwin/find_symbol_darwin.c
@@ -1,6 +1,4 @@
 
-#include "z_symbol.h"
-
 #include <mach-o/dyld.h>
 #include <mach-o/loader.h>
 #include <mach-o/nlist.h>
@@ -13,49 +11,15 @@
 
 #include <string.h>
 
-#if defined(__LP64__)
-typedef struct mach_header_64 mach_header_t;
-typedef struct segment_command_64 segment_command_t;
-typedef struct section_64 section_t;
-typedef struct nlist_64 nlist_t;
-#define LC_SEGMENT_ARCH_DEPENDENT LC_SEGMENT_64
-#else
-typedef struct mach_header mach_header_t;
-typedef struct segment_command segment_command_t;
-typedef struct section section_t;
-typedef struct nlist nlist_t;
-#define LC_SEGMENT_ARCH_DEPENDENT LC_SEGMENT
-#endif
+#include "symbol_internal.h"
 
-void *iterateSymbolTable(struct mach_header *header, const char *name);
-
-void *ZzFindSymbol(const char *image_name, const char *symbol_name) {
-  void *result    = NULL;
-  int image_count = _dyld_image_count();
-
-  for (size_t i = 0; i < image_count; i++) {
-    const struct mach_header *header = _dyld_get_image_header(i);
-    const char *name_                = _dyld_get_image_name(i);
-    name_                            = strrchr(name_, '/') + 1;
-    if (image_name != NULL && strcmp(image_name, name_))
-      continue;
-    result = iterateSymbolTable(header, symbol_name);
-    if (result)
-      break;
-  }
-
-  return result;
-}
-
-void *iterateSymbolTable(struct mach_header *header, const char *name) {
+void get_syms_in_single_image(mach_header_t *header, uintptr_t *syms, char **strs, size_t *nsyms) {
   segment_command_t *cur_seg_cmd;
   segment_command_t *linkedit_segment   = NULL;
   segment_command_t *data_segment       = NULL;
   segment_command_t *text_segment       = NULL;
   struct symtab_command *symtab_cmd     = NULL;
   struct dysymtab_command *dysymtab_cmd = NULL;
-
-  section_t *section;
 
   uintptr_t cur = (uintptr_t)header + sizeof(mach_header_t);
   for (int i = 0; i < header->ncmds; i++, cur += cur_seg_cmd->cmdsize) {
@@ -83,16 +47,53 @@ void *iterateSymbolTable(struct mach_header *header, const char *name) {
   uintptr_t linkedit_base = (uintptr_t)slide + linkedit_segment->vmaddr - linkedit_segment->fileoff;
   nlist_t *symtab         = (nlist_t *)(linkedit_base + symtab_cmd->symoff);
   char *strtab            = (char *)(linkedit_base + symtab_cmd->stroff);
+  size_t symtab_count     = symtab_cmd->nsyms;
 
-  for (int i = 0; i < symtab_cmd->nsyms; i++) {
-    if (symtab[i].n_value) {
-      uint32_t strtab_offset = symtab[i].n_un.n_strx;
-      char *tmp_symbol_name  = strtab + strtab_offset;
+  *nsyms = symtab_count;
+  *syms  = symtab;
+  *strs  = strtab;
+}
+
+void *iterateSymbolTable(char *name, nlist_t *syms, size_t nsyms, char *strs) {
+  for (uint32_t i = 0; i < nsyms; i++) {
+    if (syms[i].n_value) {
+      uint32_t strtab_offset = syms[i].n_un.n_strx;
+      char *tmp_symbol_name  = strs + strtab_offset;
       // TODO: what you want !!!
       if (strcmp(tmp_symbol_name, name) == 0) {
-        return (void *)(symtab[i].n_value + slide);
+        return (void *)(syms[i].n_value);
       }
     }
   }
   return NULL;
+}
+
+void *ZzFindSymbol(const char *image_name, const char *symbol_name) {
+  void *result    = NULL;
+  int image_count = _dyld_image_count();
+
+  for (size_t i = 0; i < image_count; i++) {
+    const struct mach_header *header = _dyld_get_image_header(i);
+    uintptr_t slide                  = _dyld_get_image_vmaddr_slide(i);
+    const char *name_                = _dyld_get_image_name(i);
+    name_                            = strrchr(name_, '/') + 1;
+    if (image_name != NULL && strcmp(image_name, name_))
+      continue;
+
+    size_t nsyms  = 0;
+    nlist_t *syms = 0;
+    char *strs    = 0;
+
+    if (is_addr_in_dyld_shared_cache(header, 0))
+      get_syms_in_dyld_shared_cache(header, &syms, &strs, &nsyms);
+    else
+      get_syms_in_single_image(header, &syms, &strs, &nsyms);
+
+    result = iterateSymbolTable(symbol_name, syms, nsyms, strs);
+    result = (uintptr_t)result + slide;
+    if (result)
+      break;
+  }
+
+  return result;
 }
