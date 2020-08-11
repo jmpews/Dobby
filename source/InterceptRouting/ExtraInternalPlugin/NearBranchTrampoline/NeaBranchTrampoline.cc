d71a4a6322890a65897f281bf88f9909e49cf0ce
jmpews <jmpews@gmail.com>
2020-08-11 15:35:10 +0800

    [core] mark func as exported

--- source/InterceptRouting/ExtraInternalPlugin/NearBranchTrampoline/NeaBranchTrampoline.cc
+++ source/InterceptRouting/ExtraInternalPlugin/NearBranchTrampoline/NeaBranchTrampoline.cc
@@ -7,13 +7,13 @@
 
 using namespace zz;
 
-void dobby_enable_near_branch_trampoline() {
+PUBLIC void dobby_enable_near_branch_trampoline() {
   RoutingPlugin *plugin = new NearBranchTrampolinePlugin;
   ExtraInternalPlugin::registerPlugin("near_branch_trampoline", plugin);
   ExtraInternalPlugin::near_branch_trampoline = plugin;
 }
 
-void dobby_disable_near_branch_trampoline() {
+PUBLIC void dobby_disable_near_branch_trampoline() {
   RoutingPlugin *plugin = (RoutingPlugin *)ExtraInternalPlugin::near_branch_trampoline;
   delete plugin;
   ExtraInternalPlugin::near_branch_trampoline = NULL;
