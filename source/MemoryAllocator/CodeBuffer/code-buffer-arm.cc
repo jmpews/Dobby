542a092bd9781cc62d468198bc06cdca45030cb0
jmpews <jmpews@gmail.com>
2020-12-14 15:30:49 +0800

    [bug-fix] fix arm stm instruction illegal alignment fatal

--- source/MemoryAllocator/CodeBuffer/code-buffer-arm.cc
+++ source/MemoryAllocator/CodeBuffer/code-buffer-arm.cc
@@ -1,7 +1,10 @@
 #include "common/macros/platform_macro.h"
 #if defined(TARGET_ARCH_ARM)
 
+#include <string.h>
+
 #include "MemoryAllocator/CodeBuffer/code-buffer-arm.h"
+
 
 arm_inst_t CodeBuffer::LoadARMInst(int offset) {
   return *reinterpret_cast<arm_inst_t *>(buffer + offset);
@@ -16,7 +19,11 @@ thumb2_inst_t CodeBuffer::LoadThumb2Inst(int offset) {
 };
 
 void CodeBuffer::RewriteAddr(int offset, addr32_t addr) {
+  memcpy(buffer + offset, &addr, sizeof(addr));
+#if 0
+  // SIGBUS (signal SIGBUS: illegal alignment)
   *reinterpret_cast<addr32_t *>(buffer + offset) = addr;
+#endif
   return;
 }
 
@@ -31,7 +38,11 @@ void CodeBuffer::RewriteThumb1Inst(int offset, thumb1_inst_t instr) {
 }
 
 void CodeBuffer::RewriteThumb2Inst(int offset, thumb2_inst_t instr) {
+  memcpy(buffer + offset, &instr, sizeof(instr));
+#if 0
+  // SIGBUS (signal SIGBUS: illegal alignment)
   *reinterpret_cast<thumb2_inst_t *>(buffer + offset) = instr;
+#endif
   return;
 }
 
@@ -51,15 +62,22 @@ void CodeBuffer::EmitThumb1Inst(thumb1_inst_t instr) {
 
 void CodeBuffer::EmitThumb2Inst(thumb2_inst_t instr) {
   ensureCapacity(getSize() + sizeof(thumb2_inst_t));
+  memcpy(buffer_cursor, &instr, sizeof(instr));
+#if 0
+  // SIGBUS (signal SIGBUS: illegal alignment)
   *reinterpret_cast<thumb2_inst_t *>(buffer_cursor) = instr;
+#endif
   buffer_cursor += sizeof(thumb2_inst_t);
-
   return;
 }
 
 void CodeBuffer::Emit32(int32_t data) {
   ensureCapacity(getSize() + sizeof(int32_t));
+  memcpy(buffer_cursor, &data, sizeof(data));
+#if 0
+  // SIGBUS (signal SIGBUS: illegal alignment)
   *reinterpret_cast<int32_t *>(buffer_cursor) = data;
+#endif
   buffer_cursor += sizeof(int32_t);
   return;
 }
