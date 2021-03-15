09b02f446de7041b388904c1d2c8bdc208e42fa0
vvb2060 <vvb2060@gmail.com>
2021-03-15 12:57:17 +0800

    Use prefab for android

--- auto-build.sh
+++ auto-build.sh
@@ -88,11 +88,10 @@ echo "prepare build ${output_dir_name}"
 
 cmake -S . -B ${output_dir_name} -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_SYSTEM_NAME=Android -DCMAKE_ANDROID_ARCH_ABI="arm64-v8a" -DCMAKE_ANDROID_NDK=$ANDROID_NDK_DIR -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang \
-  -DDOBBY_GENERATE_SHARED=OFF -DDOBBY_DEBUG=OFF
+  -DDOBBY_GENERATE_SHARED=OFF -DDOBBY_DEBUG=OFF -DPlugin.Android.BionicLinkerRestriction=ON
 cmake --build ${output_dir_name} --parallel 4 --target dobby
 
-mkdir -p ${summary_output_dir_name}/android/arm64
-mv ${output_dir_name}/${android_library_name} ${summary_output_dir_name}/android/arm64/${android_library_name}
+mv ${output_dir_name}/${android_library_name} "prefab/modules/dobby/libs/android.arm64-v8a/${android_library_name}"
 
 # build android armv7
 output_dir_name=auto-build-workspace/android-armv7-build
@@ -101,11 +100,10 @@ echo "prepare build ${output_dir_name}"
 
 cmake -S . -B ${output_dir_name} -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_SYSTEM_NAME=Android -DCMAKE_ANDROID_ARCH_ABI="armeabi-v7a" -DCMAKE_ANDROID_NDK=$ANDROID_NDK_DIR -DCMAKE_SYSTEM_VERSION=16 -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang \
-  -DDOBBY_GENERATE_SHARED=OFF -DDOBBY_DEBUG=OFF
+  -DDOBBY_GENERATE_SHARED=OFF -DDOBBY_DEBUG=OFF -DPlugin.Android.BionicLinkerRestriction=ON
 cmake --build ${output_dir_name} --parallel 4 --target dobby
 
-mkdir -p ${summary_output_dir_name}/android/armv7
-mv ${output_dir_name}/${android_library_name} ${summary_output_dir_name}/android/armv7/${android_library_name}
+mv ${output_dir_name}/${android_library_name} "prefab/modules/dobby/libs/android.armeabi-v7a/${android_library_name}"
 
 # build android x86
 output_dir_name=auto-build-workspace/android-x86-build
@@ -114,11 +112,10 @@ echo "prepare build ${output_dir_name}"
 
 cmake -S . -B ${output_dir_name} -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_SYSTEM_NAME=Android -DCMAKE_ANDROID_ARCH_ABI="x86" -DCMAKE_ANDROID_NDK=$ANDROID_NDK_DIR -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang \
-  -DDOBBY_GENERATE_SHARED=OFF -DDOBBY_DEBUG=OFF
+  -DDOBBY_GENERATE_SHARED=OFF -DDOBBY_DEBUG=OFF -DPlugin.Android.BionicLinkerRestriction=ON
 cmake --build ${output_dir_name} --parallel 4 --target dobby
 
-mkdir -p ${summary_output_dir_name}/android/x86
-mv ${output_dir_name}/${android_library_name} ${summary_output_dir_name}/android/x86/${android_library_name}
+mv ${output_dir_name}/${android_library_name} "prefab/modules/dobby/libs/android.x86/${android_library_name}"
 
 # build android x86_64
 output_dir_name=auto-build-workspace/android-x86_64-build
@@ -127,11 +124,18 @@ echo "prepare build ${output_dir_name}"
 
 cmake -S . -B ${output_dir_name} -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_SYSTEM_NAME=Android -DCMAKE_ANDROID_ARCH_ABI="x86_64" -DCMAKE_ANDROID_NDK=$ANDROID_NDK_DIR -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang \
-  -DDOBBY_GENERATE_SHARED=OFF -DDOBBY_DEBUG=OFF
+  -DDOBBY_GENERATE_SHARED=OFF -DDOBBY_DEBUG=OFF -DPlugin.Android.BionicLinkerRestriction=ON
 cmake --build ${output_dir_name} --parallel 4 --target dobby
 
-mkdir -p ${summary_output_dir_name}/android/x86_64
-mv ${output_dir_name}/${android_library_name} ${summary_output_dir_name}/android/x86_64/${android_library_name}
+mv ${output_dir_name}/${android_library_name} "prefab/modules/dobby/libs/android.x86_64/${android_library_name}"
+
+# zip android prefab
+mkdir -p prefab/modules/dobby/include
+cp "include/dobby.h" "prefab/modules/dobby/include/"
+cp "builtin-plugin/BionicLinkerRestriction/bionic_linker_restriction.h" "prefab/modules/dobby/include/"
+cp "builtin-plugin/SymbolResolver/dobby_symbol_resolver.h" "prefab/modules/dobby/include/"
+cp "prefab/AndroidManifest.xml" .
+zip -r ${summary_output_dir_name}/android_prefab.aar prefab AndroidManifest.xml -x prefab/AndroidManifest.xml
 
 if [ $DOBBY_BUILD_OUTPUT_NAME ]; then
   tar czvf ${DOBBY_BUILD_OUTPUT_NAME} ${summary_output_dir_name}
