d3579eaa8ff484c6c1e8e0f9207b3c12f5e0b120
jmpews <jmpews@gmail.com>
2021-04-29 16:18:40 +0800

    Update list structure

--- source/Interceptor.cpp
+++ source/Interceptor.cpp
@@ -2,59 +2,54 @@
 
 #include "dobby_internal.h"
 
-Interceptor *      Interceptor::priv_interceptor_ = nullptr;
+Interceptor *Interceptor::priv_interceptor_ = nullptr;
 
-Interceptor *Interceptor::SharedInstance() { 
-  if (Interceptor::priv_interceptor_ == NULL) {
-    Interceptor::priv_interceptor_          = new Interceptor();
+Interceptor *Interceptor::SharedInstance() {
+  if (Interceptor::priv_interceptor_ == nullptr) {
+    Interceptor::priv_interceptor_ = new Interceptor();
     INIT_LIST_HEAD(&Interceptor::priv_interceptor_->hook_entry_list_);
   }
   return Interceptor::priv_interceptor_;
 }
 
-HookEntryListNode *Interceptor::FindHookEntryNode(void *address) {
-  HookEntry *entry = NULL;
-
-  struct list_head *node  = NULL;
-  for (node = hook_entry_list_.next;  node != &hook_entry_list_; node = node->next) {
-    if(((HookEntryListNode *)node)->info.target_address == address) {
-      return (HookEntryListNode *)node;
+HookEntryNode *Interceptor::find_hook_entry_node(void *address) {
+  HookEntryNode *entry_node = nullptr;
+  list_for_each_entry(entry_node, &hook_entry_list_, list_node) {
+    HookEntry *entry = entry_node->entry;
+    if (entry->instruction_address == address) {
+      return entry_node;
     }
   }
-
-  return NULL;
+  return nullptr;
 }
 
 HookEntry *Interceptor::FindHookEntry(void *address) {
-  HookEntryListNode *node = NULL;
-  node = FindHookEntryNode(address);
-  if(node)
-    return &node->info;
+  HookEntryNode *entry_node = nullptr;
+  entry_node = find_hook_entry_node(address);
+  if(entry_node) {
+    return entry_node->entry;
+  }
 
-  return NULL;
+  return nullptr;
 }
 
-
-
 void Interceptor::AddHookEntry(HookEntry *entry) {
-  HookEntryListNode *node = new HookEntryListNode ;
-  node->info = *entry;
-  list_add((struct list_head *)node, &hook_entry_list_);
+  HookEntryNode *entry_node = new HookEntryNode;
+  entry_node->entry = entry;
+
+  list_add(&entry_node->list_node, &hook_entry_list_);
 }
 
 void Interceptor::RemoveHookEntry(void *address) {
-  HookEntryListNode *node = NULL;
-  node = FindHookEntryNode(address);
-  if(node) {
-    list_del((struct list_head *)node);
+  if(HookEntryNode *entry_node = find_hook_entry_node(address)) {
+    list_del(&entry_node->list_node);
   }
 }
 
 int Interceptor::GetHookEntryCount() {
   int count = 0;
-
-  struct list_head *node  = &hook_entry_list_;
-  while((node = node->next) != &hook_entry_list_) {
+  HookEntryNode *entry_node = nullptr;
+  list_for_each_entry(entry_node, &hook_entry_list_, list_node) {
     count += 1;
   }
   return count;
--- source/Interceptor.h
+++ source/Interceptor.h
@@ -3,57 +3,12 @@
 
 #include "dobby_internal.h"
 
-// List utilty
-
-struct list_head {
-  struct list_head *next;
-  struct list_head *prev;
-};
-#define container_of(ptr, type, member)                                                                                \
-  ({                                                                                                                   \
-    const __typeof(((type *)0)->member) *__mptr = (ptr);                                                               \
-    (type *)((char *)__mptr - offsetof(type, member));                                                                 \
-  })
-
-#define list_entry(ptr, type, member) container_of(ptr, type, member)
-
-#define INIT_LIST_HEAD(ptr)                                                                                            \
-  do {                                                                                                                 \
-    (ptr)->next = (ptr);                                                                                               \
-    (ptr)->prev = (ptr);                                                                                               \
-  } while (0)
-
-static inline int list_empty(struct list_head *head)
-{
-  return head->next == head;
-}
-
-static void __list_add(struct list_head *new_node, struct list_head *prev, struct list_head *next) {
-  next->prev = new_node;
-  new_node->next = next;
-  new_node->prev = prev;
-  prev->next = new_node;
-}
-
-static inline void list_add(struct list_head *new_node, struct list_head *head) {
-  __list_add(new_node, head, head->next);
-}
-
-static inline void __list_del(struct list_head *prev, struct list_head *next) {
-  next->prev = prev;
-  prev->next = next;
-}
-
-static inline void list_del(struct list_head *entry) {
-  __list_del(entry->prev, entry->next);
-  entry->next = NULL;
-  entry->prev = NULL;
-}
+#include "include/list_structure.h"
 
 typedef struct {
-  struct list_head list;
-  HookEntry info;
-} HookEntryListNode;
+  struct list_head list_node;
+  HookEntry *entry;
+} HookEntryNode;
 
 class Interceptor {
 public:
@@ -71,7 +26,7 @@ private:
   Interceptor() {
   }
 
-  HookEntryListNode *FindHookEntryNode(void *address);
+  HookEntryNode *find_hook_entry_node(void *address);
 
 private:
   struct list_head hook_entry_list_;
