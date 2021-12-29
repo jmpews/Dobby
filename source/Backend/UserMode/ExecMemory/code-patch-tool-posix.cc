f538e15e6df4aca3295245df9a6f0f218f0fa445
Bob Pan <pxb1988@gmail.com>
2021-12-29 12:31:06 +0800

    cross page

--- source/Backend/UserMode/ExecMemory/code-patch-tool-posix.cc
+++ source/Backend/UserMode/ExecMemory/code-patch-tool-posix.cc
@@ -11,21 +11,24 @@ PUBLIC MemoryOperationError CodePatch(void *address, uint8_t *buffer, uint32_t b
 
   int page_size = (int)sysconf(_SC_PAGESIZE);
   uintptr_t page_align_address = ALIGN_FLOOR(address, page_size);
-  int offset = (uintptr_t)address - page_align_address;
+
+  uintptr_t end = ALIGN_FLOOR((uintptr_t )address + buffer_size - 1, page_size);
+  int width = end - page_align_address + page_size;
+
 
 #if defined(__ANDROID__) || defined(__linux__)
 
   // change page permission as rwx
-  mprotect((void *)page_align_address, page_size, PROT_READ | PROT_WRITE | PROT_EXEC);
+  mprotect((void *)page_align_address, width, PROT_READ | PROT_WRITE | PROT_EXEC);
 
   // patch buffer
-  memcpy((void *)((addr_t)page_align_address + offset), buffer, buffer_size);
+  memcpy(address, buffer, buffer_size);
 
   // restore page permission
-  mprotect((void *)page_align_address, page_size, PROT_READ | PROT_EXEC);
+  mprotect((void *)page_align_address, width, PROT_READ | PROT_EXEC);
 #endif
 
-  addr_t clear_start_ = (addr_t)page_align_address + offset;
+  addr_t clear_start_ = (addr_t)address;
   ClearCache((void *)clear_start_, (void *)(clear_start_ + buffer_size));
 
   return kMemoryOperationSuccess;
