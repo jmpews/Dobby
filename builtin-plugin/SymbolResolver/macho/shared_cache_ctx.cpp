95a4bdb38196aed60b94b130b2c29a8ca05d8085
pudge62 <70063806+pudge62@users.noreply.github.com>
2023-11-02 16:56:15 +0800

    fix shared cache path

--- builtin-plugin/SymbolResolver/macho/shared_cache_ctx.cpp
+++ builtin-plugin/SymbolResolver/macho/shared_cache_ctx.cpp
@@ -63,7 +63,7 @@ int shared_cache_load_symbols(shared_cache_ctx_t *ctx) {
     localSymbolsOffset = ctx->mmap_shared_cache->localSymbolsOffset;
   } else {
     // iphoneos < 15.0, which has no .symbols file
-    auto mmapSharedCacheMng = new MmapFileManager(shared_cache_symbols_path);
+    auto mmapSharedCacheMng = new MmapFileManager(shared_cache_path);
 
     auto runtime_shared_cache = ctx->runtime_shared_cache;
     uint64_t mmap_length = runtime_shared_cache->localSymbolsSize;
